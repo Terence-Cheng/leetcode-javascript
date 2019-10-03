72ebfbc7f391040aa096f7ac6a12cb46ccbec704
chengtingsen <chengtingsen@chunyu.me>
2019-10-03 16:51:43 +0800

    21合并有序列表

--- src/LinkedList/MergeTwoSortedLists_21.js
+++ src/LinkedList/MergeTwoSortedLists_21.js
@@ -1,3 +1,56 @@
 /*
 * 两个有序链表的合并
 * */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+  let p1 = l1;
+  let p2 = l2;
+  const result = new ListNode(null);
+  let resultP = result;
+
+  function createNextNode(val) {
+    resultP.next = new ListNode(val)
+    resultP = resultP.next;
+  }
+
+  while (true) {
+    if (p1 && p2) {
+      if (p1.val <= p2.val) {
+        createNextNode(p1.val)
+        p1 = p1.next;
+      } else {
+        createNextNode(p2.val)
+        p2 = p2.next;
+      }
+    } else if (p1) {
+      createNextNode(p1.val)
+      p1 = p1.next;
+    } else if (p2) {
+      createNextNode(p2.val)
+      p2 = p2.next;
+    } else {
+      break;
+    }
+  }
+
+  return result.next;
+};
+
+export default mergeTwoLists
---
+++ test/LinkedList/MergeTwoSortedLists_21.test.js
@@ -0,0 +1,60 @@
+import mergeTwoLists from '../../src/LinkedList/MergeTwoSortedLists_21'
+import LinkedList from '../../lib/LinkedList/index'
+
+test('1->2->4, 1->3->4', () => {
+  const linkedList1 = new LinkedList()
+  linkedList1.push(1).push(2).push(4)
+
+  const linkedList2 = new LinkedList()
+  linkedList2.push(1).push(3).push(4)
+
+  let result = mergeTwoLists(linkedList1.getHead(), linkedList2.getHead())
+
+  const nums = [];
+  nums.push(result.val)
+  while (result.next) {
+    result = result.next
+    nums.push(result.val)
+  }
+
+  expect(nums).toEqual([1,1,2,3,4,4])
+})
+
+
+test('2', () => {
+  const linkedList1 = new LinkedList()
+  linkedList1.push(2)
+
+  const linkedList2 = new LinkedList()
+
+  let result = mergeTwoLists(linkedList1.getHead(), linkedList2.getHead())
+
+  const nums = [];
+  nums.push(result.val)
+  while (result.next) {
+    result = result.next
+    nums.push(result.val)
+  }
+
+  expect(nums).toEqual([2])
+})
+
+
+test(' , ', () => {
+  const linkedList1 = new LinkedList()
+
+  const linkedList2 = new LinkedList()
+
+  let result = mergeTwoLists(linkedList1.getHead(), linkedList2.getHead())
+
+  const nums = [];
+  if (result) {
+    nums.push(result.val)
+  }
+  while (result && result.next) {
+    result = result.next
+    nums.push(result.val)
+  }
+
+  expect(nums).toEqual([])
+})
